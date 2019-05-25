fa4f9d1811f9e779baac09ab337bb1e8af2a3129
saprtag <47823846+saprtag@users.noreply.github.com>
2019-05-25 19:11:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 console.clear;
 var hover = document.getElementsByClassName("boding")[0];
+var arrow = document.getElementsByClassName("arrow")[0];
+var span = document.getElementById('inf');
+var block = document.getElementById("block");
 
-document.getElementsByClassName('boding')[0].onclick = function(){
+document.getElementsByClassName('arrow')[0].onclick = function(){
   hover.classList.toggle('hot');
   console.log("hot");
+  arrow.classList.toggle("rotate");
+  console.log('rotate');
   
+}
+span.onclick = function(){
+  block.classList.toggle("transform");
+  console.log('transform');
+  span.classList.toggle("text")
 }
